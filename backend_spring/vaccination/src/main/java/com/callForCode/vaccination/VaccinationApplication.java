0674c623dd1a9effcb963b0326f21d9431174a74
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-07-26 17:28:24 -0300

    Criação do projeto Spring

---
+++ backend_spring/vaccination/src/main/java/com/callForCode/vaccination/VaccinationApplication.java
@@ -0,0 +1,13 @@
+package com.callForCode.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationApplication.class, args);
+	}
+
+}
---
+++ backend_spring/vaccination/src/test/java/com/callForCode/vaccination/VaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callForCode.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
