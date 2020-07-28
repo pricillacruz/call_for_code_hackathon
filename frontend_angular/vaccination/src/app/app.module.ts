29b323841495bfdbeb8a8a272497ba62662aae65
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2020-07-28 13:14:02 -0300

    criar componentes footer, nav e home

--- frontend_angular/vaccination/src/app/app.module.ts
+++ frontend_angular/vaccination/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
