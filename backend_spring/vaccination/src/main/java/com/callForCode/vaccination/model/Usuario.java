531899e62891576691748a0ed2cb467331111112
Vick <vicknogueira199@gmail.com>
2020-07-31 21:01:46 -0300

    back atualizado

--- backend_spring/vaccination/src/main/java/com/callForCode/vaccination/model/Usuario.java
+++ backend_spring/vaccination/src/main/java/com/callForCode/vaccination/model/Usuario.java
@@ -35,7 +35,7 @@ public class Usuario {
 	private String rg;
 
 	@Column(name = "cpf_usuario", unique = true)
-	@Size(min = 11, max = 11)
+//	@Size(min = 11, max = 11)
 	@CPF
 	private String cpf;
 
